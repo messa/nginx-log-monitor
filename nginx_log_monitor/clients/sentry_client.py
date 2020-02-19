2bf61cb4e717eb79884a0c2ef18f2e80ba605dcf
Petr Messner <petr.messner@gmail.com>
2020-02-19 10:19:22 +0000

    don't fail if sentry sdk not installed

--- nginx_log_monitor/clients/sentry_client.py
+++ nginx_log_monitor/clients/sentry_client.py
@@ -18,15 +18,17 @@ class SentryClient:
         client = self._clients_by_dsn.get(dsn)
         if client is None:
             if sentry_sdk is None:
-                raise Exception('sentry_sdk not installed')
+                logger.warning('sentry_sdk is not installed')
+                return None
             client = sentry_sdk.Client(dsn)
             self._clients_by_dsn[dsn] = client
         return client
 
     async def report(self, dsn, event):
         client = self._get_client(dsn)
-        event_id = client.capture_event(event)
-        if event_id:
-            logger.info('Sentry event id: %s', event_id)
-        else:
-            logger.warning('Event discarded')
+        if client:
+            event_id = client.capture_event(event)
+            if event_id:
+                logger.info('Sentry event id: %s', event_id)
+            else:
+                logger.warning('Event discarded')
